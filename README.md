2f0dd0dc75da791e434f29582c2600ade85730d4
Pete Beardmore <pete.beardmore@msn.com>
2014-03-21 17:01:57 +0000

    initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+
+## zEdzPic (the editor's pic)
+
+a simple text editor with a picture toggle for instant (re-)inspiration
+
+written in c/gtk+, and using glade for the ui/widget layout
+
+### requirements
+- gtk+-3.0+
+- GNU Make
+- pkg-config
+
+### status (pre-alpha)
+- ui renders, but basic functionality / workflow of an editor as yet unimplemented so it is totally useless
+
---
+++ app.c
@@ -0,0 +1,38 @@
+
+#include <gtk/gtk.h>
+
+int main(int argc, char **argv)
+{
+  GtkBuilder *builder;
+  GtkWidget  *window;
+  GError     *error = NULL;
+  gchar name[] = "zedzpic";
+
+  /* init GTK+ */
+  gtk_init(&argc, &argv);
+
+  /* Create new GtkBuilder object */
+  builder = gtk_builder_new();
+  /* load ui file */
+  if(!gtk_builder_add_from_file(builder, "./ui.ui", &error))
+  {
+    g_warning("%s", error->message);
+    g_free(error);
+    return(1);
+  }
+
+  /* get main window pointer */
+  window = GTK_WIDGET(gtk_builder_get_object(builder, "w_zedzpic"));
+
+  /* connect signals */
+  g_signal_connect(window, "destroy", G_CALLBACK (gtk_main_quit), NULL);
+
+  /* destroy builder */
+  g_object_unref(G_OBJECT(builder));
+
+  /* show window and start main loop */
+  gtk_widget_show(window);
+  gtk_main();
+
+  return(0);
+}
